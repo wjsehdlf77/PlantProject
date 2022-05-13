35c4219bc8663a965c8e9eea4feb8fb30052a123
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-05-13 22:33:19 +0900

    navi

--- app/src/main/java/com/example/tourguide/MainActivity.kt
+++ app/src/main/java/com/example/tourguide/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.tourguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.tourguide.Fragment.HomeFragment
+import com.example.tourguide.Fragment.MyPageFragment
+import com.example.tourguide.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var HomeFragment: Fragment
+    private lateinit var  MyPageFragment: Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
