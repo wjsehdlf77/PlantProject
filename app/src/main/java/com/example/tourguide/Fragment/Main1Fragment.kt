3ddd45c73c95f7d54ab5d0d9fc2103b983768c97
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-05-17 22:03:30 +0900

    camera

--- app/src/main/java/com/example/tourguide/Fragment/Main1Fragment.kt
+++ app/src/main/java/com/example/tourguide/Fragment/Main1Fragment.kt
@@ -1,17 +1,31 @@
 package com.example.tourguide.Fragment
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.graphics.Matrix
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.example.tourguide.MainActivity
 import com.example.tourguide.R
 import com.example.tourguide.databinding.FragmentMain1Binding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class Main1Fragment : Fragment() {
+    lateinit var filePath: String
     private var _binding: FragmentMain1Binding? = null
     private val binding get() = _binding!!
 
@@ -31,7 +45,130 @@ class Main1Fragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val requestGalleryLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        )
+        {
+            try {
+                val uri = it.data!!.data!!
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    val source = ImageDecoder.createSource(mainActivity.contentResolver, uri)
+                    ImageDecoder.decodeBitmap(source)?.let {
+                        val bitmap = resizeBitmap(it, 900f, 0f)
+                        val btp = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                        binding.imageView.setImageBitmap(btp)
+
+                    }
+                } else {
+                    MediaStore.Images.Media.getBitmap(mainActivity.contentResolver, uri)?.let {
+                        val bitmap = resizeBitmap(it, 900f, 0f)
+                        val btp = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                        binding.imageView.setImageBitmap(btp)
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+
+        binding.btnGallary.setOnClickListener {
+            //gallery app........................
+            val intent = Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            )
+            intent.type = "image/*"
+            requestGalleryLauncher.launch(intent)
+        }
+
+        val requestCameraFileLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        )
+        {
+            try {
+
+
+                val file = File(filePath)
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    val source =
+                        ImageDecoder.createSource(mainActivity.contentResolver, Uri.fromFile(file))
+                    ImageDecoder.decodeBitmap(source)?.let {
+                        val bitmap = resizeBitmap(it, 900f, 0f)
+                        val btp = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                        binding.imageView.setImageBitmap(btp)
+
+                    }
+                } else {
+                    MediaStore.Images.Media.getBitmap(
+                        mainActivity.contentResolver,
+                        Uri.fromFile(file)
+                    )
+                        ?.let {
+                            val bitmap = resizeBitmap(it, 900f, 0f)
+                            val btp = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                            binding.imageView.setImageBitmap(btp)
+
+                        }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        binding.btnCamera.setOnClickListener {
+
+            val file = imageFile()
+            filePath = file.absolutePath
+            val photoURI: Uri = FileProvider.getUriForFile(
+                mainActivity,
+                "com.example.tourguide.fileprovider", file
+            )
+
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            requestCameraFileLauncher.launch(intent)
+        }
+    }
+
+    private fun imageFile(): File {
+        val timeStamp: String =
+            SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? =
+            mainActivity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        var file = File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        )
+        return file
+
+    }
+
+    private fun resizeBitmap(src: Bitmap, size: Float, angle: Float): Bitmap {
+        val width = src.width
+        val height = src.height
+        var newWidth = 0f
+        var newHeight = 0f
+        if (width > height) {
+            newWidth = size
+            newHeight = height.toFloat() * (newWidth / width.toFloat())
+        } else {
+            newHeight = size
+            newWidth = width.toFloat() * (newHeight / height.toFloat())
+        }
+        val scaleWidth = newWidth.toFloat() / width
+        val scaleHeight = newHeight.toFloat() / height
+        val matrix = Matrix()
+        matrix.postRotate(angle);
+        matrix.postScale(scaleWidth, scaleHeight)
+        val resizedBitmap = Bitmap.createBitmap(src, 0, 0, width, height, matrix, true)
+        return resizedBitmap
+    }
 
 
 }
