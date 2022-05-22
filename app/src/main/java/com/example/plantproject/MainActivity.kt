57dc141ec2262598312c2e52616e83f27f0f45e4
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-05-22 13:38:37 +0900

    BackPressed

--- app/src/main/java/com/example/plantproject/MainActivity.kt
+++ app/src/main/java/com/example/plantproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.plantproject
 import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.FragmentTransaction
 
 
@@ -10,10 +11,13 @@ import com.example.plantproject.Adapter.FragmentViewPager
 import com.example.plantproject.NaviFragment.HomeFragment
 
 import com.example.plantproject.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    var mBackWait:Long = 0
 
 
 
@@ -28,6 +32,17 @@ class MainActivity : AppCompatActivity() {
         binding.pager.adapter = FragmentViewPager(this)
 
 
+    }
+
+    override fun onBackPressed() {
+        // 뒤로가기 버튼 클릭
+        if(System.currentTimeMillis() - mBackWait >=2000 ) {
+            mBackWait = System.currentTimeMillis()
+//            Snackbar.make(binding.pager,"뒤로가기 버튼을 한번 더 누르면 종료됩니다.",Snackbar.LENGTH_SHORT).show()
+            Toast.makeText(this, "뒤로가기 버튼을 한번 더 누르면 종료됩니다", Toast.LENGTH_SHORT).show()
+        } else {
+            finish() //액티비티 종료
+        }
     }
 
 }
